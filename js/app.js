dc20b7077c3d667dd25ba07f7fbfcbe8c09e30dd
Aeone Singson <aeone.singson@gmail.com>
2017-07-14 07:45:39 -0700

    added hamburger icon + click functionality + hide and show links

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ function content(title, image, text, url){
   this.url = url;
 }
 
-// TODO: Create a responsive primary nav
-// need hamburger bar
-// need li of nav items
+// TODO: hide primary-nav in mobile mode and show when hamburger clicked
+if($('#hamburger')){
+  $('#hamburger').on('click', function(){
+    $('#nav-list').show();
+    $('#hamburger').hide();
+  });
+}
