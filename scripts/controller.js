2caf1d3a3e51af6ff1874bb4906444c97d552d1a
Aeone Singson <aeone.singson@gmail.com>
2017-07-16 14:04:01 -0700

    js: organized js to have MVC + attempted view functionality to render project article to DOM

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,15 +1,7 @@
 'use strict';
 
- // TODO: js architecture
-
-function content(title, image, text, url){
-  this.title = title;
-  this.image = image;
-  this.text = text;
-  this.url = url;
-}
-
 // TODO: tweak this so it works like you want it to
+// TODO: this is a controller
 function toggleHamburger(){
   $('#hamburger').on('click', function(){
     $('#nav-list').show();
---
+++ scripts/projectView.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var projects = [];
+
+// TODO: js architecture
+function Project(projectData){
+  this.title = projectData.title;
+  this.image = projectData.image;
+  this.body = projectData.body;
+}
+
+Project.prototype.toHtml = function (){
+  var $newProject = $('article.project').clone();
+  $newProject.find('.project-title').text(this.title);
+  $newProject.find('.project-image').attr('src', this.image);
+  $newProject.find('.project-body').html(this.body);
+
+  return $newProject;
+}
+
+projectDataArray.forEach(function(projectDataObj){
+  projects.push(new Project(projectDataObj));
+});
+
+projects.forEach(function(project){
+  $('#project').append(project.toHtml());
+});
