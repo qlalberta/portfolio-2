8834cb74ea246d5f82ab8ded52e3b7ac2565cb99
Aeone Singson <aeone.singson@gmail.com>
2017-07-18 09:42:31 -0700

    restructured data as JSON for projects as test + moved to separate folder with volunteerData to be structured later

---
+++ data/volunteerData.js
@@ -0,0 +1,12 @@
+var volunteerDataArray = [
+  {
+    orgName: 'Organization Name',
+    role: 'My Role',
+    body: '<p>Cupcake ipsum dolor sit amet toffee. Gingerbread jelly beans candy gummies chocolate cake oat cake tiramisu. Dragée gummies chupa chups. Candy canes tootsie roll gummi bears marshmallow. Wafer croissant pie gummies ice cream ice cream. Marzipan donut caramels. Dragée sesame snaps ice cream. Chupa chups jujubes jelly-o cake dessert.</p>'
+  },
+  {
+    orgName: 'Organization Name',
+    role: 'My Role',
+    body: '<p>Sensors indicate no shuttle or other ships in this sector. According to coordinates, we have travelled 7,000 light years and are located near the system J-25. Tractor beam released, sir. Force field maintaining our hull integrity. Damage report? Sections 27, 28 and 29 on decks four, five and six destroyed. Without our shields, at this range it is probable a photon detonation could destroy the Enterprise.</p>'
+  }
+]
